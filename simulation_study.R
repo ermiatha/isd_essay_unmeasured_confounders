97472380b78e9fef8ce331476a0f5e654c1e2dc2
ermiatha <153196342+ermiatha@users.noreply.github.com>
2024-12-12 21:00:21 +0100

    Draft structure for simulation study

--- simulation_study.R
+++ simulation_study.R
@@ -1,1 +1,14 @@
+# Simulation Study
+# 12.12.2024
+# Ermioni Athanasiadi
 
+
+# Step 1: Specify Data to be Generated
+
+# Step 2: Determine data to be Varied 
+
+# Step 3: Identify relevant result measures
+
+# Step 4: Set simulation parameters
+
+# Step 5: determine analysis methods for Unmeasured Confounding (UC)
