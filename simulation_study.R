2d3d17c73b0826aaa0af5e206c9ea13dd044f225
ermiatha <153196342+ermiatha@users.noreply.github.com>
2024-12-15 17:59:47 +0100

    generate data for sensitivity analysis

--- simulation_study.R
+++ simulation_study.R
@@ -1,14 +1,108 @@
 # Simulation Study
-# 12.12.2024
+# 15.12.2024
 # Ermioni Athanasiadi
 
+library(tidyverse)
+library(ggplot2)
 
-# Step 1: Specify Data to be Generated
 
-# Step 2: Determine data to be Varied 
+# Step 1: Generate Data for sensitivity analysis with e-value
 
-# Step 3: Identify relevant result measures
+set.seed(456)
 
-# Step 4: Set simulation parameters
+sim <- 1000  # number of simulations
+N <- 100  # sample size per simulation
 
-# Step 5: determine analysis methods for Unmeasured Confounding (UC)
+# Coefficients
+beta0 <- 0.1
+beta1 <- 0.7  # Coefficient for treatment (X)
+beta_ux <- 0.8  # Coefficient for confounder (U) on X
+beta2 <- 1.9  # Coefficient for confounder (U) on Y
+
+results <- data.frame(
+  sim = integer(sim),
+  coef_x = numeric(sim),
+  coef_x_u = numeric(sim),
+  rr = numeric(sim),
+  e_value = numeric(sim)
+)
+
+for (i in 1:sim) {
+  # Unmeasured confounder U
+  u <- rbinom(N, 1, 0.5)
+  
+  # treatment X based on U
+  prob_x <- plogis(beta_ux * u)
+  x <- rbinom(N, 1, prob_x)
+  
+  # Binary outcome Y based on X, U, and random error
+  error <- rnorm(N)
+  y_prob <- plogis(beta0 + beta1 * x + beta2 * u + error)
+  y <- rbinom(N, 1, prob = y_prob)
+  
+  df <- data.frame(id = 1:N, x, y, y_prob, u)
+  
+  m1 <- glm(y ~ x, data = df, family = "binomial")  # Without U
+  m2 <- glm(y ~ x + u, data = df, family = "binomial")  # With U
+  
+  cont_tab <- table(df$y, df$x)
+  A <- cont_tab[1]
+  B <- cont_tab[3]
+  C <- cont_tab[2]
+  D <- cont_tab[4]
+  
+  # Compute relative risk (RR)
+  RR <- (A / (A + B)) / (C / (C + D))
+  # RR for E-value: assumed to be equivalent between U = 0 and U = 1
+  
+  # results
+  results[i, ] <- c(
+    sim = i,
+    coef_x = coef(m1)["x"],  # Coefficient for X without U
+    coef_x_u = coef(m2)["x"],  # Coefficient for X with U
+    rr = RR,  # Relative risk
+    e_value = RR + sqrt(RR * (RR-1))
+  )
+}
+
+# look at results
+summary(results)
+hist(results$e_value)
+
+
+# Average estimates
+mean_coef_x <- mean(results$coef_x)
+mean_coef_x_u <- mean(results$coef_x_u)
+mean_rr <- mean(results$rr)
+mean_e_value <- mean(results$e_value, na.rm = T)  # 18 NAs, negative sqrt
+
+# ATE for binary data?
+
+### Determine what to vary
+# strength of confounder
+# type of confounder: binary, categorical, continuous
+# sample size
+
+
+# Add Measured covariates 
+gender <- rbinom(N, 1, 0.55)
+age <- sample(18:55, N, replace = T)
+edu <- rbinom(N, 1, 0.5)
+
+
+# Step 2: Account for unmeasured confounding using IV (instrumental variable)
+
+# Dataset with variable correlated with X but not y
+iv <- rbinom(N, 1, prob_x)
+
+# Step 3: Account for unmeasured confounding using DiD
+
+# repeated measured data structure
+
+df$post <- 0
+# df <- df %>% mutate(id=row_number())
+df2 <- df %>% mutate(post=1)
+df <- rbind(df, df2)
+
+
+
